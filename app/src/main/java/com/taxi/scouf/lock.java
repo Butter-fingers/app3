26519a59a36a9283d2d2f3d781789883c0e809de
Butter-fingers <ebuangkamogelo@gmail.com>
2021-07-13 17:46:37 +0200

    07/13/01

--- app/src/main/java/com/taxi/scouf/lock.java
+++ app/src/main/java/com/taxi/scouf/lock.java
@@ -60,6 +60,7 @@ public class lock extends AppCompatActivity {
 
 
         drivers = getIntent().getStringArrayListExtra("drivers");
+        ArrayList<String> clo = (ArrayList<String>) drivers.clone();
         position = getIntent().getExtras().getInt("item");
         switches = new Switch[drivers.size()];
         number_line = 1;
@@ -74,7 +75,52 @@ public class lock extends AppCompatActivity {
         floatingActionButton.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
-                for (int i = 0 ; i< drivers.size(); i++) {
+
+                // new way, if checked get the string checked
+                //after take the strings checked and search for them in the array once found take the string in the array and
+                //put it last
+                //**brilliant idea**//
+                //get checked drivers
+                ArrayList<String> checked2 = new ArrayList<>();
+                for(int k = 0; k < drivers.size(); k++) {
+                    if(switches[k].isChecked()) {
+                        checked2.add(drivers.get(k));
+                    }
+                }
+
+                //resort the array using checked and keep changing the array
+                ArrayList<String> list_;
+                for(int x = 0; x < checked2.size(); x++) {
+                    list_ = new ArrayList<>();
+                    String checked_ = checked2.get(x);
+                    for(int p = 0; p < drivers.size(); p++) {
+                       if(checked_.equals(drivers.get(p))){
+                           //change the array
+                           int lo_ = drivers.size() - (p+1);
+
+                           for(int c = 0; c < drivers.size(); c++ ){
+                               if ((c != p )&& (c < p) ) {
+                                   list_.add(drivers.get(c));
+                               } else if (c > p) {
+                                   list_.add(drivers.get(c));
+                               }
+
+                           }
+                           list_.add(checked_);
+
+                       }
+                    }
+
+                    drivers = (ArrayList<String>) list_.clone();
+                }
+
+
+
+                //ArrayList<String> clone_ = (ArrayList<String>) drivers.clone();
+
+
+
+                /*for (int i = 0 ; i< drivers.size(); i++) {
 
                     dialog.show();
                     drivers_clone = (ArrayList<String>) drivers.clone();
@@ -94,6 +140,7 @@ public class lock extends AppCompatActivity {
                         int ALF = AL - i;
                         int outF = i - number;
 
+                        //check if there is a checked switch
                         if (checked > 0) {
                             oneO = drivers_clone2.get(i - checked).toString();
 
@@ -129,13 +176,13 @@ public class lock extends AppCompatActivity {
                         drivers_clone2 = (ArrayList<String>) drivers_clone.clone();
                         checked++;
                     }
-                }
+                }*/
 
                 Intent intent = new Intent(getApplicationContext(), Dashboard.class);
-                intent.putStringArrayListExtra("new", drivers_clone);
+                intent.putStringArrayListExtra("new", drivers);
                 intent.putExtra("item", position);
                 Database db = new Database(getApplicationContext());
-                db.delete(drivers_clone, position);
+                db.delete(drivers, position);
                 progressDialog.cancel();
                 startActivity(intent);
                 finish();
